24003c21806ad581f457be3547fa1af2f2aa24d8
Payam <payam.nj@gmail.com>
2026-05-23 19:14:09 +0400

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,5 +3,5 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://mindful-shift.github.io',
+    site: 'https://dearnervoussystem.shop/',
 });
