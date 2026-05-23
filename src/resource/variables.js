d5555dfcf36f6fa68740d923e98ae4570ad2305d
Payam <payam.nj@gmail.com>
2026-05-23 15:38:49 +0400

    astro config

--- src/resource/variables.js
+++ src/resource/variables.js
@@ -1,2 +1,3 @@
 export const enableSubscribeJS = false; // Set to false to disable the subscribe.js script
-export const nextShip = "July 1st";
+
+export const nextShip = "July 1st"; // Update this date as needed for the next batch shipping date
