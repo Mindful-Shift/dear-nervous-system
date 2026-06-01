529da12c71a6e4cbf9341362b2298e234778c585
Payam <payam.nj@gmail.com>
2026-06-01 18:55:25 +0400

    Add stripe links

--- src/resource/variables.js
+++ src/resource/variables.js
@@ -1,3 +1,3 @@
-export const enableSubscribeJS = false; // Set to false to disable the subscribe.js script
+export const enableSubscribeJS = true; // Set to false to disable the subscribe.js script
 
 export const nextShip = "July 1st"; // Update this date as needed for the next batch shipping date
